895610e46bef8e52838d2848acf95f7d98ef2d13
Parikirui <isaac.kirui@student.moringaschool.com>
2021-09-05 12:45:55 +0300

    added the tearDown function as well as some modification

--- credentials_test.py
+++ credentials_test.py
@@ -1,2 +1,28 @@
 import unittest
 from credentials import Info
+
+class TestInfo(unittest.TestCase):
+    def setUp(self):
+        self.new_info =Info("isaac.kirui","kirui.isaac")
+    def test_init(self):
+        self.assertEqual(self.new_info.face_bookp,"isaac.kirui")
+        self.assertEqual(self.new_info.email_p,"kirui.isaac")
+    def test_save_info(self):
+        '''
+        to test if user info is saved
+        '''
+        self.new_info.save_info()
+        self.assertEqual(len(Info.info_list),1)
+    def tearDown(self):
+        Info.info_list = []
+    def test_delete_info(self):
+        self.new_info.save_info()
+        test_info = Info("agens.89","agnes.89")
+        test_info.save_info()
+        test_info.delete_info()
+        self.assertEqual(len(Info.info_list),1)
+    def test_display_creds(self):
+        self.assertEqual(Info.display_info(),Info.info)
+
+if __name__ == '__main__':
+    unittest.main()
